6e9795e4598388129dab97c265d783c780894839
mimi313 <fugakureha@hotmail.co.jp>
2021-02-25 18:07:19 -0700

    W7D3

--- ChinookDemoSolution/WebApp/SamplePages/ManagePlaylist.aspx.cs
+++ ChinookDemoSolution/WebApp/SamplePages/ManagePlaylist.aspx.cs
@@ -20,14 +20,61 @@ namespace WebApp.SamplePages
             TracksSelectionList.DataSource = null;
         }
 
-        
+        #region Error Handling
+        protected void SelectCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+        protected void InsertCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been added.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+
+        protected void UpdateCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been updated.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void DeleteCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been removed.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        #endregion
 
         protected void ArtistFetch_Click(object sender, EventArgs e)
         {
-            
-                //code to go here
+            TracksBy.Text = "Artist";
 
-          }
+            if (string.IsNullOrEmpty(ArtistName.Text))
+            {
+                MessageUserControl.ShowInfo("Artist Search", "No artist name was supplied.");
+            }
+            //Hidden filed SearchArg is accessed by the property Value, not Text
+            SearchArg.Value = ArtistName.Text;
+
+            //To cause an ODS to re-execute to refresh the list view, you only need to do a .DataBind() against the control
+            TracksSelectionList.DataBind();
+        }
 
 
         protected void GenreFetch_Click(object sender, EventArgs e)
@@ -39,9 +86,15 @@ namespace WebApp.SamplePages
 
         protected void AlbumFetch_Click(object sender, EventArgs e)
         {
+            TracksBy.Text = "Album";
 
-                //code to go here
+            if (string.IsNullOrEmpty(AlbumTitle.Text))
+            {
+                MessageUserControl.ShowInfo("Album Search", "No album name was supplied.");
+            }
+            SearchArg.Value = AlbumTitle.Text;
 
+            TracksSelectionList.DataBind();
         }
 
         protected void PlayListFetch_Click(object sender, EventArgs e)
